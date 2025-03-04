6fb50b6f5a022586fd07dcb31e7958d87829bb55
Duclos Corentin <dvclos.corentin@gmail.com>
2025-03-04 10:58:55 +0100

    Ajout Style Inscription

--- php/connexion.php
+++ php/connexion.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion VolunTime</title>
+    <link rel="stylesheet" href="/VolunTime/VolumnTime/css/styleCo.css">
 </head>
 <body>
     <header>
-
+        <h1>Connexion</h1>
     </header>
     <form action="identification.php" method="POST">
         <div class="form-group">
