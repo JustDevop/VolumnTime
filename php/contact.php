a78764704bb27e01d350e655000a69991966e6c1
Duclos Corentin <dvclos.corentin@gmail.com>
2025-03-13 11:21:33 +0100

    Ajout de contact.php, styleContact et de l'icone plus

---
+++ php/contact.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/VolunTime/VolumnTime/css/styleContact.css" >
+    <title>VolumnTime</title>
+</head>
+<body>
+    <header>
+        <nav class="burger">
+            <span class="hamburger">☰</span>
+            <ul>
+                <li><a href="php/conversation.php">Discussions</a></li>
+                <li><a href="">Favoris</a></li>
+                <li><a href="php/dashboard.php">Contacts</a></li>
+            </ul>
+        </nav>
+        <div class="profil">
+            <img class="iconeProfil" src="/VolunTime/VolumnTime/asset/icones/icone-profil.png">
+        </div>
+        <div class="loupe">
+            <img class="iconeLoupe" src="/VolunTime/VolumnTime/asset/icones/icone-loupe.png">
+        </div>
+    </header>
+    
+    <script src="/VolunTime/VolumnTime/Js/menuhamburger.js"></script>
+
+    <div class="fav">
+        <h1>Contacts</h1>
+        <button class="add-contact"><img src="/VolunTime/VolumnTime/asset/icones/icone-plus.png" alt="Add Contact"></button>
+    </div>
+
+    <section>
+        <div class="musique">
+            <p>Benjamin</p>
+            <p>Corentin</p>
+            <p>Justine</p>
+            <p>Noémie</p>
+            <p>Shirley</p>
+        </div>
+    </section>
+</body>
+</html>
