d1f9b72c3d2d1d03c05f3dc4a459f127ae6df733
JustDevop <stackjustleb.pro@gmail.com>
2025-03-20 09:29:40 +0100

    amélioration de la page favoris

--- include/connect_bdd.php
+++ include/connect_bdd.php
@@ -1,11 +1,5 @@
 <?php
     session_start();
-try
-    {
-        $db = new PDO('mysql:host=localhost;dbname=volumntime' , 'root' , '',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    }
-catch (Exception $e)
-     {
-         die ('Erreur : ' . $e->getMessage ( )) ;
-    }
-
+    try{$db=new PDO('mysql:host=localhost;dbname=volumntime','root','');}
+    catch(Exception $e){ die('Erreur:' .$e->getMessage() );}
+?>
