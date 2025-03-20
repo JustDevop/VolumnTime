5c2ac2d32d62dffd10f873f9f1a53a16f063de24
JustDevop <stackjustleb.pro@gmail.com>
2025-03-20 15:09:23 +0100

    update connect bdd

--- include/connect_bdd.php
+++ include/connect_bdd.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
-    try{$db=new PDO('mysql:host=localhost;dbname=volumntime','root','');}
+    try{$db=new PDO('mysql:host=localhost;dbname=volumntime','hauleben','s7g9WG2$JwSC');}
     catch(Exception $e){ die('Erreur:' .$e->getMessage() );}
 ?>
